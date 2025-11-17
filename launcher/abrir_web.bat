ea8b28d7ae3ac58d8c8419c3827f02ed407c714e
bernacode <christiandelisoure12@gmail.com>
2025-11-17 14:37:28 -0400

    Primera versión de mi app

---
+++ launcher/abrir_web.bat
@@ -0,0 +1,37 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFDhcXg2RKGK7CPU47eTy/OW7hkIeQe86dpvI5oScL+cB60jldp8i2EZqisQVCRhMcQHmWwE9v2tQpW2BNsOgkh/xdWeZ41kxGGx7iWbDsw8+X/BnlMkW3TSx73HtmrYfxGr8W7oyHGDuz6MmMcoPnQ==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDhcXg2RKGK7CPU47eTy/OW7hkIeQe86dpvI5oScL+cB60jldp8i2EZqisQVCRhMcQHmWwE9v2tQpW2BNsOgkh/xdWeZ41kxGGx7iWbDsw8+X/BnlMkW3TSx73HznrAe0nD6TJUPF3Ho05N/PctO+BKxCA==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+REM Abre la app de Streamlit en el navegador predeterminado
+
+start https://app-conjuntos.streamlit.app
